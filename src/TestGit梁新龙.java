1943d4dfa2cb345e6503fbd0293c9a64cf9e0aa8
greywolflxl <976616211@qq.com>
2021-12-16 17:26:11 +0800

    Initial commit

---
+++ src/TestGit梁新龙.java
@@ -0,0 +1,10 @@
+public class TestGit梁新龙 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void stuInfo(String sid,String sname) {
+
+        System.out.println("学号" + sid + "姓名" + sname);
+    }
+}
